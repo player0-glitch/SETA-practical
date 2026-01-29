7225d0f7adb847613e50cf70a6b7cc06754cf9c3
Tlhalefo Mokhadi <TMokhadi@SINGULAR.CO.ZA>
2026-01-29 14:42:29 +0200

    feat: reversed engineered aleady existing db to get models

--- myproject/myproject/models.py
+++ myproject/myproject/models.py
@@ -9,17 +9,17 @@ from django.db import models
 
 
 class Products(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    price = models.DecimalField(db_column='Price', max_digits=18, decimal_places=2)  # Field name made lowercase.
-    productname = models.TextField(db_column='ProductName', db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
-    description = models.CharField(db_column='Description', max_length=200, db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
-    imageurl = models.TextField(db_column='ImageUrl', db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
-    quantity = models.IntegerField(db_column='Quantity')  # Field name made lowercase.
-    isavailable = models.BooleanField(db_column='IsAvailable')  # Field name made lowercase.
-    suppliername = models.CharField(db_column='SupplierName', max_length=30, db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
-    supplierid = models.IntegerField(db_column='SupplierId')  # Field name made lowercase.
-    categoryname = models.TextField(db_column='CategoryName', db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
-    categoryid = models.ForeignKey('Categories', models.DO_NOTHING, db_column='CategoryId')  # Field name made lowercase.
+    id = models.AutoField(db_column='Id', primary_key=True)
+    price = models.DecimalField(db_column='Price', max_digits=18, decimal_places=2)  
+    productname = models.TextField(db_column='ProductName', db_collation='SQL_Latin1_General_CP1_CI_AS')  
+    description = models.CharField(db_column='Description', max_length=200, db_collation='SQL_Latin1_General_CP1_CI_AS')  
+    imageurl = models.TextField(db_column='ImageUrl', db_collation='SQL_Latin1_General_CP1_CI_AS')  
+    quantity = models.IntegerField(db_column='Quantity')  
+    isavailable = models.BooleanField(db_column='IsAvailable')   
+    suppliername = models.CharField(db_column='SupplierName', max_length=30, db_collation='SQL_Latin1_General_CP1_CI_AS')  
+    supplierid = models.IntegerField(db_column='SupplierId')  
+    categoryname = models.TextField(db_column='CategoryName', db_collation='SQL_Latin1_General_CP1_CI_AS')  
+    categoryid = models.ForeignKey('Categories', models.DO_NOTHING, db_column='CategoryId')  
 
     class Meta:
         managed = False
@@ -27,13 +27,13 @@ class Products(models.Model):
 
 
 class Users(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    name = models.CharField(db_column='Name', unique=True, max_length=450, db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
-    passwordhashed = models.TextField(db_column='PasswordHashed', db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
-    email = models.TextField(db_column='Email', db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
-    roleid = models.IntegerField(db_column='RoleId')  # Field name made lowercase.
-    rolename = models.TextField(db_column='RoleName', db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
-    rolesid = models.ForeignKey('Roles', models.DO_NOTHING, db_column='RolesId', blank=True, null=True)  # Field name made lowercase.
+    id = models.AutoField(db_column='Id', primary_key=True)  
+    name = models.CharField(db_column='Name', unique=True, max_length=450, db_collation='SQL_Latin1_General_CP1_CI_AS')  
+    passwordhashed = models.TextField(db_column='PasswordHashed', db_collation='SQL_Latin1_General_CP1_CI_AS')  
+    email = models.TextField(db_column='Email', db_collation='SQL_Latin1_General_CP1_CI_AS')  
+    roleid = models.IntegerField(db_column='RoleId')  
+    rolename = models.TextField(db_column='RoleName', db_collation='SQL_Latin1_General_CP1_CI_AS')  
+    rolesid = models.ForeignKey('Roles', models.DO_NOTHING, db_column='RolesId', blank=True, null=True)  
 
     class Meta:
         managed = False
@@ -41,13 +41,13 @@ class Users(models.Model):
 
 
 class Wallets(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    transactionid = models.IntegerField(db_column='TransactionId')  # Field name made lowercase.
-    amount = models.DecimalField(db_column='Amount', max_digits=18, decimal_places=2)  # Field name made lowercase.
-    balance = models.DecimalField(db_column='Balance', max_digits=18, decimal_places=2)  # Field name made lowercase.
-    timestamp = models.DateTimeField(db_column='TimeStamp')  # Field name made lowercase.
-    userid = models.IntegerField(db_column='UserId', blank=True, null=True)  # Field name made lowercase.
-    type = models.TextField(db_column='Type', db_collation='SQL_Latin1_General_CP1_CI_AS')  # Field name made lowercase.
+    id = models.AutoField(db_column='Id', primary_key=True)  
+    transactionid = models.IntegerField(db_column='TransactionId')  
+    amount = models.DecimalField(db_column='Amount', max_digits=18, decimal_places=2)  
+    balance = models.DecimalField(db_column='Balance', max_digits=18, decimal_places=2)  
+    timestamp = models.DateTimeField(db_column='TimeStamp')  
+    userid = models.IntegerField(db_column='UserId', blank=True, null=True)  
+    type = models.TextField(db_column='Type', db_collation='SQL_Latin1_General_CP1_CI_AS')  
 
     class Meta:
         managed = False
